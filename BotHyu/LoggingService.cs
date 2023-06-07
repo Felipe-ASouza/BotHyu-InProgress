acebce5acefa8bb8ea6b4a90427109a38938e59b
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-07 00:52:37 -0300

    Bot discord em CSharp v1

---
+++ BotHyu/LoggingService.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands; // Importa o namespace do Discord
+using Discord.WebSocket;               // Importa o namespace do Discord WebSocket
+
+public class LoggingService
+{
+    public LoggingService(DiscordSocketClient client, CommandService command)
+    {
+        client.Log += LogAsync;          // Atribui o método LogAsync ao evento Log do cliente DiscordSocketClient
+        command.Log += LogAsync;         // Atribui o método LogAsync ao evento Log do comando CommandService
+    }
+
+    private Task LogAsync(LogMessage message)   // Método assíncrono LogAsync que trata as mensagens de log
+    {
+        if (message.Exception is CommandException cmdException)   // Verifica se a mensagem de log é uma exceção de comando
+        {
+            Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases.First()}"
+                              + $" failed to execute in {cmdException.Context.Channel}.");   // Imprime uma mensagem indicando a falha na execução do comando
+            Console.WriteLine(cmdException);   // Imprime a exceção do comando
+        }
+        else 
+            Console.WriteLine($"[General/{message.Severity}] {message}");   // Imprime uma mensagem de log geral
+
+        return Task.CompletedTask;   // Retorna uma tarefa completada
+    }
+                                                                        //representa a mensagem após a atualização.
+                                                                             // ↕
+    private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+    {
+        // Se a mensagem não estiver em cache, fazer o download dela resultará em obter uma cópia de `after`.
+        var message = await before.GetOrDownloadAsync();
+    
+        // Imprime a mensagem original (antes da atualização) e a mensagem atualizada.
+        Console.WriteLine($"{message} -> {after}");
+    }
+}
---
+++ BotHyu/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;  // Agora pode usar o método Log desta classe
+
+        //Guardar o Token (Utilizar algum método/forma para o Token nao ficar no código, por motivos de segurança)
+        var token = File.ReadAllText("C:\\Users\\Felipe\\Desktop\\C#\\BotHyu\\BotHyu\\token.txt"); //O Token do bot está sendo puxado por um arquivo .txt no computador local
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
